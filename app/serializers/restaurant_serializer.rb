aabffb70d945bb8f25af6b93ae33b3af7db6d324
Fabio Soares <fabiosoares@outlook.com.br>
2019-04-30 01:38:17 -0300

    Fix word restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -2,7 +2,7 @@ class RestaurantSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id, :name, :description, :review, :status, :delivery_tax, :state,
-    :city, :street, :neighborhood, :number, :complement,
+    :city, :street, :neighbourhood, :number, :complement,
     :reference, :cep, :image_url, :category_title
 
   has_many :product_categories, if: -> { @instance_options[:product_categories]}
